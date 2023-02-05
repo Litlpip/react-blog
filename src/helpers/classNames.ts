1c2faecd8531a8565b08aa9e02973621a491be63
Litlpip <ramirbukharv@gmail.com>
2023-02-05 17:11:41 +0300

    fix

--- src/helpers/classNames.ts
+++ src/helpers/classNames.ts
@@ -1,3 +1,4 @@
 export function classNames(cls:string, mods:Record<string, boolean | string>={}, additional:string[]=[]):string {
+
     return [cls,...Object.entries(mods).filter(([_, value])=>Boolean(value)).map(([key,_])=>key),...additional].join(' ')
 }
